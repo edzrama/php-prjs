24cbf6098e9a144ddcc4523b361067c888bb26bf
edzrama <edz.rama@gmail.com>
2022-04-30 11:43:30 +0800

    complete site

---
+++ contact.php
@@ -0,0 +1,87 @@
+<?php 
+	define("TITLE","Contact Us | Franklin's Fine Dining");
+	include('includes/header.php'); 
+
+?>
+	
+	<div id="contact">
+		<hr>
+			<h1> Get in touch with us! </h1>
+			
+			<?php 
+			
+				//check for header injections
+				function has_header_injection($str) {
+					return preg_match("/[\r\n]/", $str);
+				}
+				if(isset($_POST['contact_submit'])){
+					$name = trim($_POST['name']);
+					$email = trim($_POST['email']);
+					$msg = $_POST['message'];
+					
+					// check to see if $name or $email have header injections
+					if(has_header_injection($name) || has_header_injection($email)) {
+						die(); // kill the script if true
+					}
+					
+					if(!$name || !$email || !$msg) {
+						echo '<h4 class="error">All fields required</h4> <a href="contact.php" class="button block"> Go back and try again </a>';
+						exit;
+					}
+					
+					// add the recepient email to a variable
+					$to = "edz.rama@gmail.com";
+					// Create a subject
+					$subject = "$name sent you  a message via your contact form";
+				
+					// Construct the message
+					$message = "Name : $name\r\n";
+					$message .= "Email: $email\r\n";
+					$message .= "Message:\r\n$msg";
+					
+					// if subscribe checked
+					if(isset($_POST['subscribe']) && $_POST['subscribe'] == 'Subscribe') {
+						//add a new line to $message
+						$message .= "\r\n\r\nPlease add the $email to the mailing list";
+					}
+					
+				$message = wordwrap($message, 72);
+				
+				// set mail headers into a variable
+				$headers = "MIME-Version: 1.0\r\n";
+				$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+				$headers .= "From: $name <$email> \r\n";
+				//$headers .= "X-Priority: 1\r\n";
+				//$headers ="X-MSMail-Priority: High\r\n\r\n";
+				
+				// Send the email
+				mail($to, $subject, $message, $headers);
+
+			?>
+			<!-- show success message after email has sent -->
+			<h5>Thanks for contacting Franklin's</h5>
+			<p>Please allow 24hrs for a response.</p>
+			<p> <a href="index.php" class="button block">&laquo; Go to Home Page</a></p>
+				<?php } else { ?>
+				
+			<form method="POST" action="" id="contact-form">	
+			<label for="name">Your Name</label>
+			<input type="text" id="name" name="name">
+			
+			<label for="email">Your email</label>
+			<input type="email" id="email" name="email">
+			
+			<label for="messagee">and your message</label>
+			<textarea id="message" name="message"></textarea>
+			
+			<input type="checkbox" id="subscribe" name="subscribe" value="Subscribe">
+			<label for="subscribe">Subscribe to newsletter</label>
+			
+			<input type="submit" class="button next" name="contact_submit" value="Send Message">
+			</form>
+			<?php } ?>
+			<hr>
+	</div> <!-- contact -->
+	
+
+<?php include('includes/footer.php'); ?>
---
+++ includes/arrays.php
@@ -0,0 +1,76 @@
+<?php
+	// Nav Menu Items
+		$NavItems = array(
+						array( 
+							"slug" => "home",
+							"title" => "Home"
+						
+						),
+						array( 
+							"slug" => "people",
+							"title" => "Team"
+
+						),
+						array( 
+							"slug" => "dishes",
+							"title" => "Menu"
+						
+						),
+						array( 
+							"slug" => "contactus",
+							"title" => "Contact"
+						)
+					);
+	// Team Members
+	$teamMembers = array(
+						array( 
+							"name" => "Frankie III",
+							"position" => "Owner",
+							"bio" => "Frankie is the great-grandson of the original Franklin. He is the owner of Franklin's Fine Dining. He cooks a mean fritatta!",
+							"img" => "frankie"
+						
+						),
+						array( 
+							"name" => "Francis",
+							"position" => "General Manager",
+							"bio" => "Francis knows her stuff. The big sister of Frankie himself, she runs the show. Don't miss her Margherita Mondays!",
+							"img" => "francis"
+						),
+						array( 
+							"name" => "Carlos",
+							"position" => "Head Chef",
+							"bio" => "Carlos is the epitome of the phrase &ldquo; Don't judge a book by it's cover&rdquo; &mdash; You simply cannot find a better chef.",
+							"img" => "carlos"
+						
+						),
+						
+					);
+	// Menu Items
+	$menuItems = array(
+					"club-sandwich" => array(
+						"title" => "Club Sandwich",
+						"price" => 11,
+						"blurb" => "Bacon ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+						"drink" => "Club soda"
+					),
+					"dill-salmon" => array(
+						"title" => "Lemon &amp; Dill Salmon",
+						"price" => 18,
+						"blurb" => "Pork Belly ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+						"drink" => "Fancy Wine"
+					),
+					"super-salad" => array(
+						"title" => "The Super Salad <sup>&reg;</sup>",
+						"price" => 34,
+						"blurb" => "Veggy ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+						"drink" => "Jug o' Water"
+					),
+					"mexican-barbacoa" => array(
+						"title" => "Mexican Barbacoa",
+						"price" => 23,
+						"blurb" => "Cupcake ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+						"drink" => "Beer with a lime"
+					),
+				);
+	
+?>
